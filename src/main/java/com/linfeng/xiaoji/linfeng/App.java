55764712b0025c61dbd1fc061b372f52765cecc6
jijunhai <252150610@qq.com>
2020-06-06 17:38:53 +0800

    "sendened"

---
+++ src/main/java/com/linfeng/xiaoji/linfeng/App.java
@@ -0,0 +1,13 @@
+package com.linfeng.xiaoji.linfeng;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
